45c5a96e47a47dc2e949fef8461971b2644b0f2c
Ilia <luxa.ryabic@gmail.com>
2015-05-26 21:08:04 +0300

    Skype test changes

--- src/examples/skype.cpp
+++ src/examples/skype.cpp
@@ -12,7 +12,11 @@ int main(int argc, char **argv) {
 		// create tables, sequences and indexes
 		db.verbose = true;
 		auto ds = select<Messages>(db, Messages::Id > 100 && Messages::Id < 200);
-		std::cout << "All messages count " << ds.count() << std::endl;
+		SelectQuery q;
+		q.result("max(id)");
+		q.source(SkypeDB::Messages::table__);
+		int r1 = atoi(db.query(q)[0][0]);
+
 	} catch (Except e) {
 		std::cerr << "Error: " << e << std::endl;
 	}
