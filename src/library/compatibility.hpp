e785fd0de68319a6c3dcdfc50c2783713f5c06ba
Ilia Ryabokon <ilia@mac-tolik.scand.local>
2015-10-01 20:15:06 +0300

    Mac build is availiable. Not tested

--- src/library/compatibility.hpp
+++ src/library/compatibility.hpp
@@ -7,15 +7,17 @@
 #endif // #ifdef HAVE_MEMORY_H
 
 #ifndef HAVE_STRTOLL
-long long int strtoll(const char *nptr, char **endptr, int base);
+//long long int strtoll(const char *nptr, char **endptr, int base);
 #endif
 
 #ifndef HAVE_STRTOF
-float strtof (const char *nptr, char **endptr);
+//float strtof (const char *nptr, char **endptr);
 #endif
 
 #ifndef HAVE_LOCALTIME_R
 #ifdef WIN32
+#include <time.h>
+#elif __APPLE__
 #include <time.h>
 #endif
 struct tm *localtime_r(const time_t *clock, struct tm *result);
